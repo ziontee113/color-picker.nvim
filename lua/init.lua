7e14c5b672cf61e0179f05df06b4daeb52773e1a
ziontee113 <ziontee113@gmail.com>
2022-06-24 20:00:45 +0700

    add hsl, add a-r reload shortcut

--- lua/init.lua
+++ lua/init.lua
@@ -6,15 +6,20 @@ local api = vim.api
 local function detect_color(str)
 	local hex_pattern = "#%x%x%x%x%x%x"
 	local rgb_pattern = "rgb%(%s*%d+%s*,%s*%d+%s*,%s*%d+%s*%)"
+	local hsl_pattern = "hsl%(%s*%d+%s*,%s*%d+%s*,%s*%d+%s*%)"
 
 	local hex_match = string.match(str, hex_pattern)
 	local rgb_match = string.match(str, rgb_pattern)
+	local hsl_match = string.match(str, hsl_pattern)
 
 	if hex_match then
 		print(hex_match)
 	end
 	if rgb_match then
 		print(rgb_match)
+	end
+	if hsl_match then
+		print(hsl_match)
 	end
 end
 
@@ -23,6 +28,7 @@ end
 -- color: rgb(0,0,0)
 -- color: rgb( 0, 0, 0)
 -- color: rgb( 0  , 0 , 0)
+-- color: hsl( 0  , 0 , 0)
 
 local function get_current_line()
 	local cur_line = api.nvim_get_current_line()
@@ -30,9 +36,12 @@ local function get_current_line()
 	return cur_line
 end
 
-vim.keymap.set("n", "  k", function()
+vim.keymap.set("n", "<C-A-K>", function()
 	vim.cmd("messages clear")
 	detect_color(get_current_line())
 end, { noremap = true, silent = true })
+
+-- for quickly reload file
+vim.keymap.set("n", "<A-r>", ":luafile %<cr>", { noremap = true, silent = false })
 
 return M
