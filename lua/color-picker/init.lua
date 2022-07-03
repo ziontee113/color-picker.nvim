9ca12833ec19fe15cc55d254aeb18f5b07d46079
ziontee113 <ziontee113@gmail.com>
2022-07-03 09:54:38 +0700

    add ConvertHEXandRGB and ConvertHEXandHSL commands

--- lua/color-picker/init.lua
+++ lua/color-picker/init.lua
@@ -5,13 +5,20 @@ local utils_window = require("color-picker.utils.window")
 
 -------------------------------------
 
+M.setup = utils_window.setup
+M.convert_cursor_color = utils_window.convert_cursor_color
+
 vim.api.nvim_create_user_command("PickColor", function()
 	utils_window.pop("normal")
 end, {})
 vim.api.nvim_create_user_command("PickColorInsert", function()
 	utils_window.pop("insert")
 end, {})
-
-M.setup = utils_window.setup
+vim.api.nvim_create_user_command("ConvertHEXandRGB", function()
+	utils_window.convert_cursor_color("rgb")
+end, {})
+vim.api.nvim_create_user_command("ConvertHEXandHSL", function()
+	utils_window.convert_cursor_color("hsl")
+end, {})
 
 return M
